60d929a0cce515f02c456312b4b19fce1f0be072
mayorgamaximo <mayorgamaximo5@gmail.com>
2025-04-14 01:05:30 -0300

    game compation

--- public/src/game.js
+++ public/src/game.js
@@ -48,15 +48,70 @@ async function getRandomPokemon() {
     console.log(error);
   }
 }
+//Funcion para sacar el numero romano y devolverlo como decimal
+function decimal(obj) {
+  const equivalencias = {
+    I: 1,
+    II: 2,
+    III: 3,
+    IV: 4,
+    V: 5,
+    VI: 6,
+    VII: 7,
+    VIII: 8,
+    IX: 9
+  };
+  let nombre = obj.name.trim().replace(/generation-/i, "").trim();
+  return equivalencias[nombre.toUpperCase()] || null;
+}
+
+function comparar(pokemonRandom, pokeInfo){
+  console.log(pokemonRandom)
+  console.log(pokeInfo)
+
+  //Check de victoria
+  if(pokemonRandom[0] === pokeInfo.name ){
+    console.log("ganaste!!")
+  }
+  //comparacion de tipos
+  if (pokemonRandom[2] === pokeInfo.types[0]){
+    console.log("tipo 1 igual")
+  }
+  //revisa si existe segundo tipo
+  if(!pokeInfo.types[1]){
+    pokeInfo.types[1] = "Ninguno"
+  }
+  if (pokemonRandom[3] === pokeInfo.types[1]){
+    console.log("tipo 2 igual")
+  }
+
+  if (pokemonRandom[4] === pokeInfo.color){
+    console.log("color igual")
+  }
+
+  //convierto de numeros romanos a decimales
+  pokemonRandom[5]=decimal(pokemonRandom[5])
+
+  if (pokemonRandom[5] === pokeInfo.generation){
+    console.log("generacion igual")
+  }
+  //reviso si la altura y el peso son iguales
+  if(pokemonRandom[6]/10 === pokeInfo.height){
+    console.log("altura igual")
+  }
+  
+  if(pokemonRandom[7]/10 === pokeInfo.weight){
+    console.log("peso igual")
+  }
+}
 
 const input = document.getElementById('pokemon-input')
-input.addEventListener('keypress', async (event) => { //faltaba este async
+input.addEventListener('keypress', async (event) => { 
   if (event.key === 'Enter') { 
     const pokemonName = input.value.trim().toLowerCase()
     try {
-      const pokeinfo = await fetchPokemonData(pokemonName) //faltaba este await
-      console.log(pokeinfo)
-      console.log("Nombre:", pokeinfo.name)
+      const pokeinfo = await fetchPokemonData(pokemonName) 
+      comparar(pokemonInfo, pokeinfo)
     } catch(error){
       console.error("Error:", error)
     }
